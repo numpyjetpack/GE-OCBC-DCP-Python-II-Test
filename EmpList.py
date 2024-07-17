6b36984f6e26161c51c85138675c5583bef75eef
numpyjetpack <yeejiat@hotmail.com>
2024-07-17 10:24:30 +0800

    Update EmpList.py
    added employee

--- EmpList.py
+++ EmpList.py
@@ -1,5 +1,6 @@
 from EmpClass import Employee
 #emp4 = Employee("Lyon", "Ho", "M002")
 emp5 = Employee("Darren", "Tan", "M003")
+emp6 = Employee("Hock Seng", "Khor", "M004")
 #print(emp1.FullName, emp2.FullName, emp3.FullName, emp4.FullName)
-print(emp1.FullName, emp2.FullName, emp3.FullName, emp4.FullName, emp5.FullName)
+print(emp1.FullName, emp2.FullName, emp3.FullName, emp4.FullName, emp5.FullName, emp6.FullName)

39094cde22b303f81d1232c22daffe64edd2e79c
Ry130997 <chong.ng130997@gmail.com>
2024-07-17 10:09:26 +0800

    Update EmpList.py
    Add new employee.

--- EmpList.py
+++ EmpList.py
@@ -1,3 +1,5 @@
 from EmpClass import Employee
-emp4 = Employee("Lyon", "Ho", "M002")
-print(emp1.FullName, emp2.FullName, emp3.FullName, emp4.FullName)
+#emp4 = Employee("Lyon", "Ho", "M002")
+emp5 = Employee("Darren", "Tan", "M003")
+#print(emp1.FullName, emp2.FullName, emp3.FullName, emp4.FullName)
+print(emp1.FullName, emp2.FullName, emp3.FullName, emp4.FullName, emp5.FullName)
